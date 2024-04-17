fe65bfd41f94d8bde1641fafb147377866b1b64a
Sarah Renkhoff <sarah.renkhoff@navimatix.de>
2024-04-17 10:33:04 +0200

    Re-architecture
    * Use distinct threads for data sampling and network transmission
    * Couple thread through shared access to a ringbuffer
    * This way, data collection should never be blocked by network blockage

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,12 @@ use esp_idf_svc::{
     wifi::EspWifi,
     timer::EspTimerService,
 };
-use lsm9ds1::LSM9DS1Init;
+use lsm9ds1::{LSM9DS1Init, LSM9DS1};
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
 use hecate_protobuf as proto;
-use proto::{Acceleration, Message};
+use proto::{Message, SensorDataSample};
+use ringbuffer::{RingBuffer, AllocRingBuffer};
 
 mod wifi;
 mod ws;
@@ -64,66 +65,84 @@ fn main() -> anyhow::Result<()> {
     sensor.begin_gyro().expect("Failed to initialize gyroscope");
     sensor.begin_mag().expect("Failed to initialize magnetometer");
 
-    // Setup WiFi
+    // Create ringbuffer for sensor data
+    let sensor_data_ringbuffer = AllocRingBuffer::<proto::SensorDataSample>::new(128);
+    let sensor_data_ringbuffer_mutex = Arc::new(Mutex::new(sensor_data_ringbuffer));
+
+    // Spawn sensor sampling thread
+    let mutex_clone = sensor_data_ringbuffer_mutex.clone();
+    std::thread::Builder::new()
+        .name("sensor sampling thread".into())
+        .spawn(move ||
+            sensor_sampling_thread(sensor, mutex_clone)
+                .inspect_err(|e| log::error!("Sensor sampling thread died: {e}"))
+        ).expect("Failed to create sensor sampling thread");
+
+        
+    // Setup networking
     let wifi = EspWifi::new(p.modem, sysloop.clone(), Some(nvs.clone()))?;    
     let wifi_mutex = Arc::new(Mutex::new(wifi));
+    
+    let wifi_mutex_clone = wifi_mutex.clone();
+    let sysloop_clone = sysloop.clone();
+    let buffer_mutex_clone = sensor_data_ringbuffer_mutex.clone();
+    std::thread::Builder::new()
+        .name("networking thread".into())
+        .stack_size(16384)
+        .spawn(move ||
+            networking_thread(wifi_mutex_clone, sysloop_clone, buffer_mutex_clone)
+                .inspect_err(|e| log::error!("Networking thread died: {e}"))
+        ).expect("Failed to create networking thread");
     
     // Start WiFi indicator led
     let indicator_led = PinDriver::output(p.pins.gpio13)?;
     let wifi_mutex_clone = wifi_mutex.clone();
-    std::thread::spawn(move || wifi_indicator(indicator_led, wifi_mutex_clone).map_err(|e| {
-        log::error!("WiFi Indicator died (;ω;) ({e})");
-        anyhow::Ok(())
-    }));
+    std::thread::Builder::new()
+        .name("WiFi indicator".into())
+        .spawn(move ||
+            wifi_indicator(indicator_led, wifi_mutex_clone)
+                .inspect_err(|e| log::error!("WiFi Indicator died (;ω;) ({e})"))
+        ).expect("Failed to create wifi indicator thread");
 
+    
+    loop {
+        std::thread::sleep(Duration::from_millis(100));
+    }
+}
+
+fn networking_thread<R>(wifi_mutex: Arc<Mutex<EspWifi>>, sysloop: EspSystemEventLoop, data_buffer: Arc<Mutex<R>>) -> Result<()>
+where
+    R: RingBuffer<proto::SensorDataSample>,
+{
+    
     // Connect to WiFi
-    log::info!("Connecting to {}", CONFIG.wifi_ssid);
-    wifi::connect(wifi_mutex.clone(), CONFIG.wifi_ssid, CONFIG.wifi_psk, sysloop.clone())?;
-
-    // Open WS connection
-    let mut client = Box::new(ws::WebSocketClient::<8192>::new());
-    client.connect(CONFIG.ws_host, CONFIG.ws_port, CONFIG.ws_endpoint)
-        .expect("Websocket client failed to connect");
-
+    log::info!("Connecting to WiFi {}", CONFIG.wifi_ssid);
+    wifi::connect(wifi_mutex.clone(), CONFIG.wifi_ssid, CONFIG.wifi_psk, sysloop.clone())
+        .inspect_err(|e| log::error!("Error during WiFi connection attempt: {}", e))?;
     log::info!("Connected");
 
-    let mut samples = Vec::<proto::SensorDataSample>::new();
-    let timer = EspTimerService::new().expect("Failed to initialize timer service");
-    let start_time = timer.now();
+    // Open WS connection
+    log::info!("Connecting to {}:{}{}", CONFIG.ws_host, CONFIG.ws_port, CONFIG.ws_endpoint);
+    let mut client = Box::new(ws::WebSocketClient::<4096>::new());
+    client.connect(CONFIG.ws_host, CONFIG.ws_port, CONFIG.ws_endpoint)
+        .expect("Websocket client failed to connect");
+    log::info!("Connected");
+
     loop {
+        let samples = data_buffer.lock()
+            .inspect_err(|e| log::error!("Failed to lock ringbuffer mutex: {}", e))
+            .ok()
+            .and_then(|mut buffer| Some(buffer.drain().take(100).collect::<Vec<_>>()));
 
-        let acc = sensor.read_accel();
-        let gyro = sensor.read_gyro();
-        let mag = sensor.read_mag();
-
-        if let (Ok(acc), Ok(gyro), Ok(mag)) = (acc, gyro, mag) {
-            let (acc_x, acc_y, acc_z) = acc;
-            let (gyro_x, gyro_y, gyro_z) = gyro;
-            let (mag_x, mag_y, mag_z) = mag;
-            let time = timer.now() - start_time;
-            
-            let sample = proto::SensorDataSample {
-                time: time.as_secs_f32(),
-                acceleration: proto::Acceleration { x: acc_x, y: acc_y, z: acc_z },
-                gyroscope: proto::GyroscopeData { x: gyro_x, y: gyro_y, z: gyro_z },
-                magnetometer: proto::MagnetometerData { x: mag_x, y: mag_y, z: mag_z },
-            };
-
-            samples.push(sample);
-        }
-
-        if samples.len() >= 25 {
+        if let Some(samples) = samples {
             let message = proto::SensorData {
                 id: "feather".into(),
-                samples: samples,
+                samples,
             }.encode_to_vec();
 
-            match client.send_binary(&message) {
-                Ok(_) => { log::info!("Sent data"); },
-                Err(e) => { log::error!("Failed to send data: {}", e); },
-            }
-
-            samples = Vec::new();
+            _ = client.send_binary(&message)
+                .inspect_err(|e| log::error!("Failed to send data: {}", e))
+                .and_then(|_| { log::info!("Sent data"); Ok(())});
         }
 
         std::thread::sleep(Duration::from_millis(100));
@@ -142,5 +161,37 @@ where
         _ = led.set_level(is_up.into());
 
         std::thread::sleep(Duration::from_millis(200));
+    }
+}
+
+fn sensor_sampling_thread<I, R>(mut sensor: LSM9DS1<I>, buffer_mutex: Arc<Mutex<R>>) -> Result<()>
+where
+    I: lsm9ds1::interface::Interface,
+    R: RingBuffer<SensorDataSample>,
+{
+    let timer = EspTimerService::new().expect("Failed to initialize timer service");
+    let start_time = timer.now();
+
+    loop {
+        let acc = sensor.read_accel();
+        let gyro = sensor.read_gyro();
+        let mag = sensor.read_mag();
+
+        if let (Ok((ax, ay, az)), Ok((gx, gy, gz)), Ok((mx, my, mz))) = (acc, gyro, mag) {
+            let time = timer.now() - start_time;
+            let sample = proto::SensorDataSample {
+                time: time.as_secs_f32(),
+                acceleration: proto::Acceleration{ x: ax, y: ay, z: az },
+                magnetometer: proto::MagnetometerData { x: mx, y: my, z: mz },
+                gyroscope: proto::GyroscopeData { x: gx, y: gy, z: gz },
+            };
+
+            match buffer_mutex.lock() {
+                Ok(mut buffer) => buffer.push(sample),
+                Err(e) => log::error!("Error locking ringbuffer mutex: {}", e),
+            }
+        }
+
+        std::thread::sleep(Duration::from_millis(10));
     }
 }
